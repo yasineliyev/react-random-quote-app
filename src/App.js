45f75c312de3b26802e1fb844e93b57c69cda4a0
Yasin <105815296+yasineliyev@users.noreply.github.com>
2023-01-08 13:24:37 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState("");
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote">
+        <p>{quotes.text}</p>
+        <p>{quotes.author}</p>
+      </div>
+      <div className="btnContainer">
+        <button onClick={getQuote} className="btn">
+          Get quote
+        </button>
+      </div>
     </div>
   );
 }
